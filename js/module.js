1b72f253acaea1548c5fd266d3b7dfce1d199585
shenjiaheng <shenjiaheng@weidian.com>
2019-01-04 16:50:03 +0800

    any

---
+++ js/module.js
@@ -0,0 +1,6 @@
+let i = 1
+function foo() {
+    console.log(++i)
+}
+module.exports.i = i
+module.exports.foo = foo
---
+++ js/useModule.js
@@ -0,0 +1,5 @@
+let newModule = require('./module')
+console.log(newModule.i)
+newModule.foo()
+newModule.foo()
+console.log(newModule.i)

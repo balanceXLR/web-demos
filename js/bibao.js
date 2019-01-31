5804db2eab7b84336f47413d024d17a26db3ef97
shenjiaheng <shenjiaheng@weidian.com>
2019-01-31 13:52:16 +0800

    bibao

--- js/bibao.js
+++ js/bibao.js
@@ -1,4 +1,24 @@
-let i = function() {
-    console.log(1)
+var name = 'window'
+// this指向当前执行函数
+var obj = {
+    name: 'my',
+    getName: function () {
+        console.log(this.name)
+        return function () {
+            return this.name
+        }
+    }
 }
-i()
+console.log(obj.getName()())
+
+
+// num标量被保存在内存中
+var countNumber = (function () {
+    var num = 0;
+    return function () {
+        console.log(++num)
+        return num;
+    };
+})();
+countNumber()
+countNumber()
